da5a3d098c057066c0429e89fa59eff8f5b4c47f
Amir Khavari <amir.khavari73@gmail.com>
2023-07-27 14:19:34 +0330

    feat(Player): Player can now move using arrow or A/D keys and jump using Space key

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] private float JumpForce = 6f;
+    [SerializeField] private float MoveForce = 2f;
+
+    private Rigidbody2D rigidbody;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbody = GetComponent<Rigidbody2D>();    
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rigidbody.velocity = new Vector2(rigidbody.velocity.x, JumpForce);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        {
+            rigidbody.velocity = new Vector2(-MoveForce, rigidbody.velocity.y);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        {
+            rigidbody.velocity = new Vector2(MoveForce, rigidbody.velocity.y);
+        }
     }
 }
