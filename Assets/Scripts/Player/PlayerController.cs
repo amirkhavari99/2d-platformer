3050e9005978a992a9f4ceeea29859a34da9a68a
Amir Khavari <amir.khavari73@gmail.com>
2023-07-27 16:18:18 +0330

    feat(Animation): Added animations of Idle, Running, Jump, and Fall to the Ninja Player

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -6,16 +6,30 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float JumpForce = 6f;
     [SerializeField] private float MoveForce = 2f;
+    [SerializeField] private float MovingEpsilon = 0.1f;
     [SerializeField] private LayerMask floor;
 
+    private SpriteRenderer sr;
     private Rigidbody2D rb;
     private BoxCollider2D boxCollider;
+    private Animator animator;
+
+    private enum PlayerState
+    {
+        Idle = 0,
+        Running = 1,
+        Jump = 2,
+        Fall = 3
+    }
+    private PlayerState currentState;
 
     // Start is called before the first frame update
     void Start()
     {
+        sr = GetComponent<SpriteRenderer>();    
         rb = GetComponent<Rigidbody2D>();    
-        boxCollider = GetComponent<BoxCollider2D>();    
+        boxCollider = GetComponent<BoxCollider2D>();
+        animator = GetComponent<Animator>();    
     }
 
     // Update is called once per frame
@@ -34,6 +48,48 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
         {
             rb.velocity = new Vector2(MoveForce, rb.velocity.y);
+        }
+
+        UpdateAnimation();
+    }
+
+    private void UpdateAnimation()
+    {
+        PlayerState state;
+        if (rb.velocity.x > MovingEpsilon)
+        {
+            state = PlayerState.Running;
+            sr.flipX = false;
+        }
+        else if (rb.velocity.x < -MovingEpsilon)
+        {
+            state = PlayerState.Running;
+            sr.flipX = true;
+        }
+        else
+        {
+            state = PlayerState.Idle;
+        }
+
+        if (rb.velocity.y > MovingEpsilon)
+        {
+            state = PlayerState.Jump;
+        }
+        else if (rb.velocity.y < -MovingEpsilon)
+        {
+            state = PlayerState.Fall;
+        }
+
+        SetAnimationState(state);
+    }
+
+    private void SetAnimationState(PlayerState state)
+    {
+        if (currentState != state)
+        {
+            Debug.LogError($"Setting Player State to {state}");
+            animator.SetInteger("playerState", (int)state);
+            currentState = state;
         }
     }
 
