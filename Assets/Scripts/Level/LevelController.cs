ec073709a9c50ce543668c25e91dda8d3d058959
Amir Khavari <amir.khavari73@gmail.com>
2023-07-27 15:19:54 +0330

    feat(Collectibles): Added Melon collectibles & UI to the game.

--- Assets/Scripts/Level/LevelController.cs
+++ Assets/Scripts/Level/LevelController.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LevelController : MonoBehaviour
 {
     [SerializeField] private PlayerController player;
     [SerializeField] private Camera mainCamera;
+    [SerializeField] Text playerScoreText;
+
+    private int playerScore = 0;
 
     private static LevelController instance = null;
     public static LevelController Instance
@@ -30,5 +34,14 @@ public class LevelController : MonoBehaviour
     public Vector2 GetPlayerPosition()
     {
         return player.GetCurrentPosition();
+    }
+
+    public void PlayerGotAPoint(int point)
+    {
+        if(point > 0)
+        {
+            playerScore += point;
+            playerScoreText.text = $"Score: {playerScore}"; 
+        }
     }
 }
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -7,12 +7,12 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float JumpForce = 6f;
     [SerializeField] private float MoveForce = 2f;
 
-    private Rigidbody2D rigidbody;
+    private Rigidbody2D rb;
 
     // Start is called before the first frame update
     void Start()
     {
-        rigidbody = GetComponent<Rigidbody2D>();    
+        rb = GetComponent<Rigidbody2D>();    
     }
 
     // Update is called once per frame
@@ -20,22 +20,31 @@ public class PlayerController : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            rigidbody.velocity = new Vector2(rigidbody.velocity.x, JumpForce);
+            rb.velocity = new Vector2(rb.velocity.x, JumpForce);
         }
 
         if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
         {
-            rigidbody.velocity = new Vector2(-MoveForce, rigidbody.velocity.y);
+            rb.velocity = new Vector2(-MoveForce, rb.velocity.y);
         }
 
         if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
         {
-            rigidbody.velocity = new Vector2(MoveForce, rigidbody.velocity.y);
+            rb.velocity = new Vector2(MoveForce, rb.velocity.y);
         }
     }
 
     public Vector2 GetCurrentPosition()
     {
         return transform.position;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Collectible"))
+        {
+            LevelController.Instance.PlayerGotAPoint(1);
+            Destroy(collision.gameObject);
+        }
     }
 }
